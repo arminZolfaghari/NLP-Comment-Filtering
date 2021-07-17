c330707517b105cba07a4155209ee5e2cd8c9364
Amirhossein Rajabpour <rajabpouramirhosein@gmail.com>
2021-07-17 23:57:16 +0430

    dictionary functions changed

--- BigramModel.py
+++ BigramModel.py
@@ -17,10 +17,7 @@ class BigramModel():
 
     def create_unary_words_dict(self):
         for sentence in self.train_positive_set:
-            words_in_sent = sentence.split(' ')
-            words_in_sent.insert(0, '<s>')
-            words_in_sent.append('</s>')
-            words_in_sent = Counter(words_in_sent)
+            words_in_sent = Counter(sentence)
             for word in words_in_sent.keys():
                 if word in self.count_unary_train_pos_dict.keys():
                     self.count_unary_train_pos_dict[word] += 1
@@ -28,10 +25,7 @@ class BigramModel():
                     self.count_unary_train_pos_dict[word] = 1
 
         for sentence in self.train_negative_set:
-            words_in_sent = sentence.split(' ')
-            words_in_sent.insert(0, '<s>')
-            words_in_sent.append('</s>')
-            words_in_sent = Counter(words_in_sent)
+            words_in_sent = Counter(sentence)
             for word in words_in_sent.keys():
                 if word in self.count_unary_train_neg_dict.keys():
                     self.count_unary_train_neg_dict[word] += 1
@@ -42,10 +36,7 @@ class BigramModel():
 
     def create_binary_words_dict(self):
         for sentence in self.train_positive_set:
-            words_in_sent = sentence.split(' ')
-            words_in_sent.insert(0, '<s>')
-            words_in_sent.append('</s>')
-            words_in_sent = Counter(words_in_sent)
+            words_in_sent = Counter(sentence)
             for word_i in range(len(words_in_sent.keys()) - 1):
                 couple_word = (list(words_in_sent.keys())[word_i], list(words_in_sent.keys())[word_i + 1])
                 if couple_word in self.count_binary_train_pos_dict.keys():
@@ -54,10 +45,7 @@ class BigramModel():
                     self.count_binary_train_pos_dict[couple_word] = 1
 
         for sentence in self.train_negative_set:
-            words_in_sent = sentence.split(' ')
-            words_in_sent.insert(0, '<s>')
-            words_in_sent.append('</s>')
-            words_in_sent = Counter(words_in_sent)
+            words_in_sent = Counter(sentence)
             for word_i in range(len(words_in_sent.keys()) - 1):
                 couple_word = (list(words_in_sent.keys())[word_i], list(words_in_sent.keys())[word_i + 1])
                 if couple_word in self.count_binary_train_neg_dict.keys():
