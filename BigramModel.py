4091aa38c724425aec6f907c5352bf03434de203
Amirhossein Rajabpour <rajabpouramirhosein@gmail.com>
2021-07-17 22:31:09 +0430

    dictionary creating functions structures changed

--- BigramModel.py
+++ BigramModel.py
@@ -11,21 +11,26 @@ class BigramModel():
         self.train_neg_dict = {}
 
 
-    def create_words_dict(self, words_dict, dataset):  # dataset is an array of sentences (each element is a sentence)
-        # dataset example = ["this is sent1", "this is sent2"]
-        for sentence in dataset:
-            print('sent: ', sentence)
+    def create_pos_words_dict(self):
+        for sentence in self.train_positive_set:
             words_in_sent = sentence.split(' ')
-            print(words_in_sent)
+            words_in_sent.insert(0, '<s>')
+            words_in_sent.append('</s>')
             words_in_sent = Counter(words_in_sent)
-            print(words_in_sent)
             for word in words_in_sent.keys():
-                print('word: ', word)
-                if word in words_dict.keys():
-                    words_dict[word] += 1
+                if word in self.train_pos_dict.keys():
+                    self.train_pos_dict[word] += 1
                 else:
-                    words_dict[word] = 1
-        return words_dict
+                    self.train_pos_dict[word] = 1
 
-
-
+    def create_neg_words_dict(self):
+        for sentence in self.train_negative_set:
+            words_in_sent = sentence.split(' ')
+            words_in_sent.insert(0, '<s>')
+            words_in_sent.append('</s>')
+            words_in_sent = Counter(words_in_sent)
+            for word in words_in_sent.keys():
+                if word in self.train_neg_dict.keys():
+                    self.train_neg_dict[word] += 1
+                else:
+                    self.train_neg_dict[word] = 1
