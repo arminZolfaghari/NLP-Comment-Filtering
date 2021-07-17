b7ac4c1047ee0b0d166f607156804525efdfc820
Amirhossein Rajabpour <rajabpouramirhosein@gmail.com>
2021-07-17 23:05:34 +0430

    create_binary_words_dict function implemented

--- BigramModel.py
+++ BigramModel.py
@@ -39,7 +39,29 @@ class BigramModel():
                     self.count_unary_train_neg_dict[word] = 1
 
     def create_binary_words_dict(self):
+        for sentence in self.train_positive_set:
+            words_in_sent = sentence.split(' ')
+            words_in_sent.insert(0, '<s>')
+            words_in_sent.append('</s>')
+            words_in_sent = Counter(words_in_sent)
+            for word_i in range(len(words_in_sent.keys()) - 1):
+                couple_word = (list(words_in_sent.keys())[word_i], list(words_in_sent.keys())[word_i + 1])
+                if couple_word in self.count_binary_train_pos_dict.keys():
+                    self.count_binary_train_pos_dict[couple_word] += 1
+                else:
+                    self.count_binary_train_pos_dict[couple_word] = 1
 
+        for sentence in self.train_negative_set:
+            words_in_sent = sentence.split(' ')
+            words_in_sent.insert(0, '<s>')
+            words_in_sent.append('</s>')
+            words_in_sent = Counter(words_in_sent)
+            for word_i in range(len(words_in_sent.keys()) - 1):
+                couple_word = (list(words_in_sent.keys())[word_i], list(words_in_sent.keys())[word_i + 1])
+                if couple_word in self.count_binary_train_neg_dict.keys():
+                    self.count_binary_train_neg_dict[couple_word] += 1
+                else:
+                    self.count_binary_train_neg_dict[couple_word] = 1
 
 
     def calculate_probability
