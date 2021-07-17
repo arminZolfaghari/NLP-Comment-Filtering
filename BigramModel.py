9c8e82de9d495f3acb8953b556bf89f07df9f30b
Armin Zolfaghari Daryani <arminzd1@gmail.com>
2021-07-17 22:12:12 +0430

    before pull

--- BigramModel.py
+++ BigramModel.py
@@ -4,9 +4,14 @@
 
 class BigramModel():
 
-    def __init__(self, train_pos_set, train_neg_set):
+    def __init__(self, train_pos_set, train_neg_set, lambda_arr):
         self.train_positive_set = train_pos_set
         self.train_negative_set = train_neg_set
+        self.lambda_arr = lambda_arr    # [h0, h1, h2]
 
+
+    def recognize_sentence(self, sentence):
+        sentence_arr = sentence.split(" ")
+        for i in range(len(sentence_arr)):
 
 
