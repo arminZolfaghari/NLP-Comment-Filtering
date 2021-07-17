5020483a7d5f5c7bae744109d6dc57d5d95c5592
Armin Zolfaghari Daryani <arminzd1@gmail.com>
2021-07-17 20:22:01 +0430

    start BigramModel.py class, add some functions in Dataset.py

---
+++ BigramModel.py
@@ -0,0 +1,13 @@
+
+
+
+
+class BigramModel():
+
+    def __init__(self, train_pos_set, train_neg_set):
+        self.train_positive_set = train_pos_set
+        self.train_negative_set = train_neg_set
+
+
+    def pre_process(self):
+
--- Dataset.py
+++ Dataset.py
@@ -1,3 +1,5 @@
+import random
+
 
 # read positive comments file and store
 def store_positive_comments_from_file():
@@ -18,3 +20,31 @@ def store_negative_comments_from_file():
 
     return negative_comment_arr
 
+
+
+def get_positive_train_test_set():
+    positive_dataset = store_positive_comments_from_file()
+    dataset_len = len(positive_dataset)
+    test_set_len = int(dataset_len * 0.1)
+    test_set = []
+    for i in range(test_set_len):
+        test = positive_dataset.pop(random.randint(0, len(positive_dataset) - 1))
+        test_set.append(test)
+    train_set = positive_dataset
+
+    return train_set, test_set
+
+
+def get_negative_train_test_set():
+    pass
+
+# remove sign character
+def pre_process():
+    pass
+
+
+def
+
+x1, x2 = get_positive_train_test_set()
+x3 = store_positive_comments_from_file()
+print(len(x1), len(x2), len(x3))
