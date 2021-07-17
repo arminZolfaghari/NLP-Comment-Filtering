538b29b31c40654489627e2dcec61d106e8edf56
Armin Zolfaghari Daryani <arminzd1@gmail.com>
2021-07-17 23:18:16 +0430

    add calculate_simple_conditional_probability

--- BigramModel.py
+++ BigramModel.py
@@ -42,9 +42,21 @@ class BigramModel():
 
 
 
-    # calculate p(wi|wi-1) = count(wi-1 wi)/count(wi-1)
-    def calculate_simple_conditional_probability(self, word1, word2):
-        if word1
+    # calculate p(wi|wi-1) = count(wi-1 wi)/count(wi-1) => p(word2|word1)
+    def calculate_simple_conditional_probability(self, word1, word2, dataset_mode):
+        if dataset_mode == "positive":
+            tuple_words = (word1, word2)
+            if word1 in self.count_unary_train_pos_dict and tuple_words in self.count_binary_train_pos_dict:
+                res = self.count_binary_train_pos_dict[tuple_words]/self.count_unary_train_pos_dict[word1]  # count(wi-1 wi)/count(wi-1)
+            else:
+                res = 0         # when word1 isn't in dict or 'word1 word2' isn't in dict
+        elif dataset_mode == "negative":
+            tuple_words = (word1, word2)
+            if word1 in self.count_unary_train_neg_dict and tuple_words in self.count_binary_train_neg_dict:
+                res = self.count_binary_train_neg_dict[tuple_words]/self.count_unary_train_neg_dict[word1]  # count(wi-1 wi)/count(wi-1)
+            else:
+                res = 0         # when word1 isn't in dict or 'word1 word2' isn't in dict
+
 
     # calculate p(wi|wi-1) = h2 * p(wi|wi-1) + h1 * p(wi) + h0 * e
     def calculate_conditional_probability(self, word1, word2):
